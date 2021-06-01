e8dd29f9026ee69e28f1246f7fcb3d2d116a2660
zenz01 <85192787+zenz01@users.noreply.github.com>
2021-06-01 16:09:03 -0300

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -3,9 +3,9 @@ systemctl start firewalld
 firewall-cmd --zone=public --add-port=3128/tcp --permanent
 firewall-cmd --reload
 cd /etc/
-wget --no-check-certificate -N https://raw.githubusercontent.com/zenz01/imp/master/3proxy.cfg?token=AUJ7AUYVJDB5UUTV2JFUH73AW2BZC
+wget --no-check-certificate -N https://raw.githubusercontent.com/zenz01/imp/master/3proxy.cfg
 chmod 600 /etc/3proxy.cfg
-wget --no-check-certificate https://raw.githubusercontent.com/zenz01/imp/master/.proxyauth?token=AUJ7AU3TFPK4HUY2N4JEZG3AW2B4S
+wget --no-check-certificate https://raw.githubusercontent.com/zenz01/imp/master/.proxyauth
 chmod 600 /etc/.proxyauth
 systemctl enable 3proxy.service
 systemctl enable firewalld.service
